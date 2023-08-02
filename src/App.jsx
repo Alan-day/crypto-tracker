0169eb33174e6bf0ee5483209a099fd3b6e34c45
Alan <lesterthenovice@gmail.com>
2023-08-02 12:32:23 +0100

    basic structure completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Pageswitch from "./Pageswitch/Pageswitch";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>_nology trader</h1>
+      <Pageswitch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pageswitch/Pageswitch.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Pageswitch = () => {
+  return (
+    <div className="pageswitch-container">
+      <button className="pageswitch-container__button">Cryptocurrency</button>
+      <button className="pageswitch-container__button">Stocks</button>
+    </div>
+  );
+};
+
+export default Pageswitch;
